19737183bc89b7b10eb713b53c17d71c624b0d9d
Gregory Kanter <gregorykanter1@gmail.com>
2024-09-27 23:33:49 +0300

    Format

--- include/chain.hpp
+++ include/chain.hpp
@@ -4,26 +4,26 @@
 
 namespace Ettuseus {
 
+class Blockchain {
 
-    class Blockchain {
+public:
+  Blockchain(double sample_rate);
+  auto add_block(std::filesystem::path &&file, double block_time,
+                 int num_of_repeats) -> void;
 
-    public:
-        Blockchain(double sample_rate);
-        auto add_block(std::filesystem::path&& file, double block_time, int num_of_repeats) -> void;
+private:
+  class Block {
+  public:
+    constexpr static int INFINITE_REPEAT = 0;
+    Block(std::filesystem::path &&file, std::uint_fast64_t time_nanoseconds,
+          int num_of_repeats);
 
-    private:
-    class Block {
-    public:
-        constexpr static int INFINITE_REPEAT = 0;
-        Block(std::filesystem::path&& file, std::uint_fast64_t time_nanoseconds, int num_of_repeats);
-
-    private:
-        std::filesystem::path file;
-        std::uint_fast64_t time_nanoseconds;
-        int num_of_repeats;
-
-    };
-        std::list<Block> chain;
-        double sample_rate;
-    };
-}
+  private:
+    std::filesystem::path file;
+    std::uint_fast64_t time_nanoseconds;
+    int num_of_repeats;
+  };
+  std::list<Block> chain;
+  double sample_rate;
+};
+} // namespace Ettuseus
--- lib/chain.cpp
+++ lib/chain.cpp
@@ -4,19 +4,15 @@
 
 namespace Ettuseus {
 
-Blockchain::Block::Block(std::filesystem::path&& file, const std::uint_fast64_t time_nanoseconds, const int num_of_repeats)
-: file(std::move(file))
-, time_nanoseconds(time_nanoseconds)
-, num_of_repeats(num_of_repeats)
-{}
+Blockchain::Block::Block(std::filesystem::path &&file,
+                         const std::uint_fast64_t time_nanoseconds,
+                         const int num_of_repeats)
+    : file(std::move(file)), time_nanoseconds(time_nanoseconds),
+      num_of_repeats(num_of_repeats) {}
 
-Blockchain::Blockchain(const double sample_rate)
-: sample_rate(sample_rate)
-{}
+Blockchain::Blockchain(const double sample_rate) : sample_rate(sample_rate) {}
 
-auto Blockchain::add_block(std::filesystem::path&& file, double block_time, int num_of_repeats) -> void
-{
+auto Blockchain::add_block(std::filesystem::path &&file, double block_time,
+                           int num_of_repeats) -> void {}
 
-}
-
-}
+} // namespace Ettuseus
